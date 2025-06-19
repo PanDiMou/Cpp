ae701358b3bf1ee5d9aa91b8788f3a638a482031
GAdda <candide-96.breveter@icloud.com>
2025-06-19 17:37:29 +0200

    Adds example code snippets for loop control
    Adds example C++ code snippets demonstrating the use
    of break statements within for loops.
    
    Also, adds the Xcode user interface state file to .gitignore
    and removes the deleted file.

---
+++ c++/c++/57_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/*int main() {
+    for (int i{0}; i <= 9; ++i) {
+        if (i == 3) {
+            break;
+        }
+        std::cout << i << std::endl;
+    }
+    return 0;
+}*/
---
+++ c++/c++/58_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*int main() {
+    for (int i{0}; i < 5; ++i) {
+        std::cout << "i = " << i << std::endl;
+        for (int j{0}; j < 3; ++j) {
+            std::cout << "j = " << j << std::endl;
+            if (j == 1) {
+                break;
+            }
+        }
+    }
+    return 0;
+}*/
